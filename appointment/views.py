726ee3d46fd26788c5d6e470f1d78d6ecb8bb633
aikrooz <aikrooz>
2026-03-05 14:22:33 +0100

    fix: correct HTML structure and add login requirement for appointment view

--- appointment/views.py
+++ appointment/views.py
@@ -3,7 +3,14 @@ from .models import AppointmentModel
 from accounts.models import DoctorProfileModel
 # Create your views here.
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import AppointmentModel, DoctorProfileModel
+
+@login_required
+def appointment(request):
+    doctors=DoctorProfileModel.objects.all()
+    if request.mehtod=="POST":
+        
 
 
 
